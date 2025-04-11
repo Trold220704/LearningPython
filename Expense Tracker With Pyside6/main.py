9ce0cb50a29a21083d3aef9650698aa17dbf43b7
Trold220704 <109646820+Trold220704@users.noreply.github.com>
2025-04-11 21:17:00 +0200

    new file:   Expense Tracker With Pyside6/__pycache__/mainwindow.cpython-313.pyc 	new file:   Expense Tracker With Pyside6/main.py 	new file:   Expense Tracker With Pyside6/mainwindow.py 	new file:   Password Manager/main.py 	new file:   Password Manager/password.csv 	modified:   Simple Gui with Pyside/main.py

---
+++ Expense Tracker With Pyside6/main.py
@@ -0,0 +1,14 @@
+from PySide6.QtWidgets import QApplication
+from mainwindow import MainWindow
+import sys
+
+
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow(app)
+    window.show()
+    app.exce()
