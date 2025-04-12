705931e12179d0ae57e51cafd748b73795cd38b0
Trold220704 <109646820+Trold220704@users.noreply.github.com>
2025-04-12 11:16:11 +0200

    deleted:    Expense Tracker With Pyside6/__pycache__/mainwindow.cpython-313.pyc 	deleted:    Expense Tracker With Pyside6/mainwindow.py 	renamed:    Expense Tracker With Pyside6/main.py -> Simple Gui with Pyside/gui with buttons/main.py 	new file:   Simple Gui with Pyside/gui with buttons/mainwindow.py 	new file:   start.png

--- Expense Tracker With Pyside6/mainwindow.py
+++
@@ -1,8 +0,0 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
-
-
-class MainWindow(QMainWindow):
-    def __init__(self, app):
-        super().__init__()
-        self.app = app
-        self.setWindowTitle("Expense Tracker")
--- Simple Gui with Pyside/gui with buttons/main.py
+++ Simple Gui with Pyside/gui with buttons/main.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow(app)
     window.show()
-    app.exce()
+    app.exec()
